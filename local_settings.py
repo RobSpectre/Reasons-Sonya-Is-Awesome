b34b3ac170e37ced205bbb3164e06a5fcadf5436
Rob Spectre <rob.spectre@gmail.com>
2012-01-20 15:09:01 -0500

    Work fucker.

--- local_settings.py
+++ local_settings.py
@@ -13,7 +13,7 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 
 # Begin Heroku configuration - configured through environment variables.
 import os
-ACCOUNT_SID = os.environ.get['ACCOUNT_SID']
-AUTH_TOKEN = os.environ.get['AUTH_TOKEN']
-SONYA_APP_SID = os.environ.get['SONYA_APP_SID']
-SONYA_CALLER_ID = os.environ.get['SONYA_CALLER_ID']
+ACCOUNT_SID = os.environ['ACCOUNT_SID']
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
+SONYA_APP_SID = os.environ['SONYA_APP_SID']
+SONYA_CALLER_ID = os.environ['SONYA_CALLER_ID']
