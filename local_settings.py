e9409120820ac5366ae22247b43ad4b14be84316
Rob Spectre <rob.spectre@gmail.com>
2012-01-20 14:48:04 -0500

    Added local settings.

---
+++ local_settings.py
@@ -0,0 +1,19 @@
+'''
+Configuration Settings
+
+Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
+'''
+
+# Uncommet to configure in file.
+#ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
+#AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
+#RAMONES_APP_SID = "APzzzzzzzzz"
+#RAMONES_CALLER_ID = "+17778889999"
+
+
+# Begin Heroku configuration - configured through environment variables.
+import os
+ACCOUNT_SID = os.environ['ACCOUNT_SID']
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
+SONYA_APP_SID = os.environ['RAMONES_APP_SID']
+SONYA_CALLER_ID = os.environ['RAMONES_CALLER_ID']
