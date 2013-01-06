47ab1ad6918a215b13e3676926554936da91fcd5
Rob Spectre <rob.spectre@gmail.com>
2013-01-06 17:08:37 -0500

    Correctly name local_settings variables.

--- local_settings.py
+++ local_settings.py
@@ -7,8 +7,8 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 # Uncommet to configure in file.
 #ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
 #AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
-#RAMONES_APP_SID = "APzzzzzzzzz"
-#RAMONES_CALLER_ID = "+17778889999"
+#SONYA_APP_SID = "APzzzzzzzzz"
+#SONYA_CALLER_ID = "+17778889999"
 
 
 # Begin Heroku configuration - configured through environment variables.
